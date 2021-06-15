8be70f6c2028dd2c754aefbef530b7c749a50dfd
Sarbjit Singh <sarbjeets18@gmail.com>
2021-06-15 08:19:37 +0100

    few changes

--- StaticVariable.java
+++ StaticVariable.java
@@ -3,5 +3,5 @@ public class StaticVariable{
 static final String url="jdbc:mysql://localhost:3306/";
 public static final String databaseName= "studentsdb";
 public static final String userName= "root";
-public static final String dbPassword="SahajSarb@4321";  
+public static final String dbPassword="";  //enter database password here    
 }
